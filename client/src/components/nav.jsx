95c341912a97b09f97709912548bba6d896c4722
Ancsak01 <tryball001@gmail.com>
2021-08-04 12:20:05 +0200

    for github pages

--- client/src/components/nav.jsx
+++ client/src/components/nav.jsx
@@ -17,26 +17,21 @@ export default class Nav extends React.Component {
         return (
             <main className="sticky-top">
                 <nav className="navbar navbar-expand-lg navbar-dark bg-dark ps-4">
-                    <a className="navbar-brand" href="/home">Navbar</a>
+                    <a className="navbar-brand" href="/home">Home</a>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
 
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav mr-auto">
-                            <li className="nav-item active">
-                                <a className="nav-link" href="/comments">Comments <span className="sr-only">(current)</span></a>
+                            <li className="nav-item ms-4">
+                                <a className="nav-link" href="/comments">Comments</a>
                             </li>
-                            <li className="nav-item">
+                            <li className="nav-item ms-4">
                                 <a className="nav-link" href="/about">About</a>
                             </li>
-                            <li className="nav-item">
+                            <li className="nav-item ms-4">
                                 <a className="nav-link" href="/events">Events</a>
-                            </li>
-                            <li className="nav-item">
-                                <form className="form-inline">
-                                    <input className="form-control" type="search" placeholder="Search" aria-label="Search" />
-                                </form>
                             </li>
                         </ul>
                     </div>
